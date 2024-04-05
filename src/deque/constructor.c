0713fe73103f69f9cfd93b15237e9affc69ec1af
bitquence <imbitquence@gmail.com>
2024-04-06 00:12:21 +0200

    eliminate dead `bzero` implementation

--- src/deque/constructor.c
+++ src/deque/constructor.c
@@ -13,22 +13,6 @@
 #include <stdlib.h>
 #include "deque.h"
 
-/*
-static void	write_bytes(void *s, unsigned char ch, size_t n)
-{
-	size_t			i;
-	unsigned char	*bytes;
-
-	i = 0;
-	bytes = (unsigned char *)s;
-	while (i < n)
-	{
-		bytes[i] = ch;
-		i++;
-	}
-}
-*/
-
 t_deque	*deque_new(size_t cap)
 {
 	t_deque	*this;
@@ -36,6 +20,10 @@ t_deque	*deque_new(size_t cap)
 
 	size_in_bytes = sizeof(t_deque) + (cap * sizeof(t_deque_data));
 	this = malloc(size_in_bytes);
-	/* write_bytes(this, '\0', size_in_bytes); */
+	*this = (t_deque){
+		.cap = cap,
+		.len = 0,
+		.head = 0,
+	};
 	return (this);
 }
