9ef36d7c6c06557c909b017b0bcca7a9976ff06e
Jonathan <imbitquence@gmail.com>
2024-08-09 17:12:22 +0200

    opt for binary search for `ranked.c`

--- src/deque/ranked.c
+++ src/deque/ranked.c
@@ -6,29 +6,40 @@
 /*   By: jamar <jamar@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/09 00:19:21 by jamar             #+#    #+#             */
-/*   Updated: 2024/08/09 00:19:23 by jamar            ###   ########.fr       */
+/*   Updated: 2024/08/09 17:03:33 by jamar            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "deque.h"
 
-static size_t	index_of(const t_deque *self, t_deque_data element)
+static size_t	index_of(const t_deque *self_sorted, t_deque_data element)
 {
-	size_t	i;
+	size_t	start;
+	size_t	midpoint;
+	size_t	end;
 
-	i = 0;
-	while (i < deque_len(self))
+	start = 0;
+	end = deque_len(self_sorted) - 1;
+	while (start <= end)
 	{
-		if (*deque_get(self, i) == element)
-			return (i);
+		midpoint = start + (end - start) / 2;
+		if (*deque_get(self_sorted, midpoint) == element)
+			return (midpoint);
+		if (*deque_get(self_sorted, midpoint) < element)
+			start = midpoint + 1;
+		else
+			end = midpoint - 1;
 	}
 	return (0);
 }
+
+#include <assert.h>
 
 void	deque_make_ranked(t_deque *self, const t_deque *self_sorted)
 {
 	// is this needed?
 	// assert (deque_is_unique(self_sorted));
+	assert (deque_len(self) == deque_len(self_sorted));
 
 	size_t			i;
 	t_deque_data	*target_element;
@@ -37,7 +48,7 @@ void	deque_make_ranked(t_deque *self, const t_deque *self_sorted)
 	while (i < deque_len(self))
 	{
 		target_element = deque_get_mut(self, i);
-		*target_element = index_of_sorted(self_sorted, *target_element);
+		*target_element = index_of(self_sorted, *target_element);
 		i++;
 	}
 }
