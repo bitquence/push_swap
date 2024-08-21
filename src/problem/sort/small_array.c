38576f8afff32d4b8233224012813f314da7ab63
bitquence <imbitquence@gmail.com>
2024-08-21 18:58:33 +0200

    implement special case sort for 3 < n <= 5

--- src/problem/sort/small_array.c
+++ src/problem/sort/small_array.c
@@ -30,16 +30,37 @@ void	sort_and_output_solution_for_three_or_less(t_state state)
 		apply_operation(state, OP_REVERSE_ROTATE_A);
 }
 
+void	push_and_sort_biggest_intergers_out(t_state state)
+{
+	int	threshold;
+
+	threshold = deque_len(state.stack_a) - 3;
+	while ((int)deque_len(state.stack_a) > 3)
+	{
+		if (*deque_get(state.stack_a, 0) < threshold)
+			apply_operation(state, OP_PUSH_B);
+		else
+			apply_operation(state, OP_ROTATE_A);
+	}
+	if (deque_len(state.stack_b) > 1 && \
+		*deque_get(state.stack_b, 0) < *deque_get(state.stack_b, 1))
+		apply_operation(state, OP_SWAP_B);
+}
+
+void	exhaust_stack_b_into_stack_a(t_state state)
+{
+	while ((int)deque_len(state.stack_b) > 0)
+		apply_operation(state, OP_PUSH_A);
+}
+
 void	sort_and_output_solution_for_small_array(t_state state)
 {
 	bool	sorting_more_than_three_integers;
 
 	sorting_more_than_three_integers = (deque_len(state.stack_a) > 3);
 	if (sorting_more_than_three_integers)
-		return ;
-		//push_smallest_numbers_out(state);
+		push_and_sort_biggest_intergers_out(state);
 	sort_and_output_solution_for_three_or_less(state);
 	if (sorting_more_than_three_integers)
-		return ;
-		//exhaust_stack_b_into_stack_a(state);
+		exhaust_stack_b_into_stack_a(state);
 }
