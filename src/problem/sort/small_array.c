0c10cc33b652d6377a08c25ef89d8c752c66a3e4
bitquence <imbitquence@gmail.com>
2024-08-21 18:58:17 +0200

    rename small array sorting method

--- src/problem/sort/small_array.c
+++ src/problem/sort/small_array.c
@@ -15,19 +15,7 @@
 
 size_t	longest_continuously_increasing_subsequence_len(const t_deque *stack);
 
-
-// accepting case:
-//  - ▂▅▇ -> ∅
-//
-// step one turns
-//  2 1 3 - ▅▂▇ -> sa
-//  1 3 2 - ▂▇▅ -> sa ra
-//  3 2 1 - ▇▅▂ -> sa rra
-//   ... into ...
-//  1 2 3 - ▂▅▇ -> ∅
-//  3 1 2 - ▇▂▅ -> ra
-//  2 3 1 - ▅▇▂ -> rra
-void	sort_and_output_solution_for_array_of_less_than_or_three(t_state state)
+void	sort_and_output_solution_for_three_or_less(t_state state)
 {
 	t_deque *stack_a;
 
@@ -50,7 +38,7 @@ void	sort_and_output_solution_for_small_array(t_state state)
 	if (sorting_more_than_three_integers)
 		return ;
 		//push_smallest_numbers_out(state);
-	sort_and_output_solution_for_array_of_less_than_or_three(state);
+	sort_and_output_solution_for_three_or_less(state);
 	if (sorting_more_than_three_integers)
 		return ;
 		//exhaust_stack_b_into_stack_a(state);
